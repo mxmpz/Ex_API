f6184fc605bc8e8a2ba4e1e63429d51366685597
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-01-06 11:26:45 +0100

    express + port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 require('dotenv').config()
 
+const express = require('express')
+const app = express()
+const port = 5000
+
 const connect = require('./data/helpers/db')
 connect()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Ok')
+})
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port)
+})
