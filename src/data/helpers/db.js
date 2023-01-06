522379f9873110b5d48d9f557c0af489998b485b
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-01-06 11:59:20 +0100

    add comment index.js

--- src/data/helpers/db.js
+++ src/data/helpers/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 // Supprime le Warning lors de la connexion
 mongoose.set('strictQuery', false)
 
-// Connexion à la base de donnée mongoose à partir des variables d'environnements
+// On crée la fonction de connexion à la base de donnée mongoose à partir des variables d'environnements
 const connect = () => {
   mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`)
     .then(() => {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 
+// Utilisataion de Express
 const express = require('express')
 const app = express()
+
+// Le serveur local est tourne sur le port 5000
 const port = 5000
 
+// On se connect à la base de donnée
 const connect = require('./data/helpers/db')
 connect()
 
+// On teste l'utilisataion de Express
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -14,6 +19,7 @@ app.get('/', (req, res) => {
   res.send('Ok')
 })
 
+// On affiche sur quel port tourne le serveur dans la console
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
 })
